7c4ec33bbae839e172b1b38081ee77c3c8772578
badrbnh <badrechahide@gmail.com>
2024-01-03 21:00:18 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,13 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [uploadPhoto(fileName), signUpUser(firstName, lastName)];
-
-  return Promise.allSettled(promises).then((results) => {
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) => (
     results.map((result) => ({
       status: result.status,
       value: result.value === 'fulfilled' ? result.value : `${result.reason}`,
-    }));
-  });
+    }))
+  ));
 }
