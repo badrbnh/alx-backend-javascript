146fd0191989132dda474a21762b7fbec5540e78
badrbnh <badrechahide@gmail.com>
2024-01-03 20:10:58 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
     .then((values) => {
-      console.log(values[1].body, values[0].firstName, values[0].lastName);
+      console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline ');
