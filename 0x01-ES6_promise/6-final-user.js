186160c435121517421d14d839f0d004201b7a3f
badrbnh <badrechahide@gmail.com>
2024-01-03 21:20:33 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,12 +3,11 @@ import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    uploadPhoto(fileName),
     signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
   ]).then((results) => (
     results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.status : `${result.reason}`,
-    }))
-  ));
+      value: result.status === 'fulfilled' ? result.value : `${result.reason}`,
+    }))));
 }
