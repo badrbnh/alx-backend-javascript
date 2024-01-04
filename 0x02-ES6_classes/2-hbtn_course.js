80d62b965a065cdfaee815495b987fe757b25d3b
badrbnh <badrechahide@gmail.com>
2024-01-04 19:18:35 +0100

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof this._name === 'string') return (this._name = name);
+    else throw new TypeError('Name must be a string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (Number.isInteger(this._length)) return (this._length = length);
+    else throw new TypeError('Length must be a number');
+  }
+
+  get students() {
+    return this._name;
+  }
+
+  set students(students) {
+    if (Array.isArray(this._students)) return (this._students = students);
+    else throw new TypeError('Students must be an array');
+  }
+}
