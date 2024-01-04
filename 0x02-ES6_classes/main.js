5a4079f754efa625b344e72833aceeb7cf97b644
badrbnh <badrechahide@gmail.com>
2024-01-04 23:16:19 +0100

    task 7

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,13 +1,5 @@
-import Building from './5-building.js';
+import Airport from "./7-airport.js";
 
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
