a12f8bec3d4f697e12c44da55e4c0b1acdabfc53
badrbnh <badrechahide@gmail.com>
2024-01-04 20:05:50 +0100

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+    constructor(code, name) {
+        if (typeof code === 'string')
+            this._code = code;
+        else
+            throw new TypeError('Code must be a string')
+        if (typeof name === 'string')
+            this._name = name;
+        else
+            throw new TypeError('Name must be a string')
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        if (typeof name === 'string')
+            this._name = name;
+        else
+            throw new TypeError('Name must be a string')
+    }
+
+    get code() {
+        if (typeof code === 'string')
+            this._code = code;
+        else
+            throw new TypeError('Code must be a string')
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`
+    }
+}
