1261355000a034ed95612fc7087349d89ddc417d
badrbnh <badrechahide@gmail.com>
2024-01-04 23:00:18 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,6 +2,8 @@ export default class Building {
   constructor(sqft) {
     if (Number.isInteger(sqft)) this._sqft = sqft;
     else throw new TypeError('Sqft must be a number');
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined)
+        throw new Error('evacuationWarningMessage must be overridden by child class');
   }
 
   get sqft() {
