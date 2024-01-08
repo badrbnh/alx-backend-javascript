32b14b458b72498f5cfb08937619af611bbb62e6
badrbnh <badrechahide@gmail.com>
2024-01-08 18:30:16 +0100

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,8 @@
 export default function getListStudents() {
-  const arr = [];
   const obj = [
     { id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Fransisco' },
   ];
-  arr.push(obj);
   return obj;
 }
