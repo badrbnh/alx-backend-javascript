64188d23ce6efa0feba270319532e2517225028c
badrbnh <badrechahide@gmail.com>
2024-01-08 19:56:02 +0100

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
-export default function updateStudentGradeByCity(arr, city, grade) {
+export default function updateStudentGradeByCity(arr, city, newGrade) {
   return arr.filter((student) => student.location === city).map((student) => {
-    const grades = grade.find((grade) => grade.studentId === student.id);
-    const newGrade = grades ? grades.grade : 'N/A';
-    return { ...student, newGrade };
+    const gradeInfo = newGrade.find((grade) => grade.studentId === student.id);
+    const grade = gradeInfo ? gradeInfo.grade : 'N/A';
+    return { ...student, grade };
   });
 }
